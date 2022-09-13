3348226399191db0e1effce63192576b2eab46d9
AliKhalaf1 <alikhalaf1234.ah@gmail.com>
2022-09-13 20:23:39 +0200

    removed commented parts

--- finalProject/src/main/java/com/example/finalProject/Controllers/UserController.java
+++ finalProject/src/main/java/com/example/finalProject/Controllers/UserController.java
@@ -1,33 +1,33 @@
-//package com.example.finalProject.Controllers;
-//
-//import com.example.finalProject.Entities.User;
-//import com.example.finalProject.Services.UserService;
-//import lombok.AllArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@AllArgsConstructor
-////@CrossOrigin(origins = "http://localhost:4200")
-//@RequestMapping(path= "/user")
-//public class UserController {
-//
-//    private final UserService userService;
-//
-//    @PostMapping("/register")
-//    public ResponseEntity<User> register(@RequestBody User user){
-//        return ResponseEntity.ok().body(userService.register(user));
-//    }
-//    @GetMapping(path = "confirm")
-//    public String confirm(@RequestParam("token") String token) {
-//        return userService.confirmToken(token);
-//    }
-//
-//    @GetMapping(path = "/test")
-//    public String test(){
-//        return "h3l";
-//    }
-//
-//
-//
-//}
+package com.example.finalProject.Controllers;
+
+import com.example.finalProject.Entities.User;
+import com.example.finalProject.Services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+//@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(path= "/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user){
+        return ResponseEntity.ok().body(userService.register(user));
+    }
+    @GetMapping(path = "confirm")
+    public String confirm(@RequestParam("token") String token) {
+        return userService.confirmToken(token);
+    }
+
+    @GetMapping(path = "/test")
+    public String test(){
+        return "h3l";
+    }
+
+
+
+}
--- finalProject/src/main/java/com/example/finalProject/Entities/User.java
+++ finalProject/src/main/java/com/example/finalProject/Entities/User.java
@@ -1,5 +1,7 @@
 package com.example.finalProject.Entities;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 //import org.springframework.security.core.GrantedAuthority;
 //import org.springframework.security.core.userdetails.UserDetails;
 
@@ -11,66 +13,66 @@ import java.util.Collection;
 @AllArgsConstructor
 @Getter
 @Setter
-public class User  {
+public class User  implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="user_id")
     private Long id;
-}
-//    @Column(name="username")
-//    private String userName;
-//    @Column(name="email")
-//    private String email;
-//    @Column(name="password")
-//    private String password;
-//    @Column(name="mobile")
-//    private String mobile;
-//    @Column(name="enabled")
-//    private Boolean enabled = false;
-//
-//    public User(String userName, String email, String password, String mobile, Boolean enabled) {
-//        this.userName = userName;
-//        this.email = email;
-//        this.password = password;
-//        this.mobile = mobile;
-//        this.enabled = enabled;
-//    }
-//    
+
+    @Column(name="username")
+    private String userName;
+    @Column(name="email")
+    private String email;
+    @Column(name="password")
+    private String password;
+    @Column(name="mobile")
+    private String mobile;
+    @Column(name="enabled")
+    private Boolean enabled = false;
+
+    public User(String userName, String email, String password, String mobile, Boolean enabled) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.mobile = mobile;
+        this.enabled = enabled;
+    }
+
 //    @Column(name="rating")
 //    @OneToMany(mappedBy = "user")
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return null;
-//    }
-//
-//    @Override
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return userName;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return false;
-//    }
-//}
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
