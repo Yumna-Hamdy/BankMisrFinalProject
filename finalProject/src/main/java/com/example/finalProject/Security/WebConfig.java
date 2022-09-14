1c6426fc024afb78a6e9b9383636d3ea89fc447c
AliKhalaf1 <alikhalaf1234.ah@gmail.com>
2022-09-14 12:08:16 +0200

    cleaning

--- finalProject/src/main/java/com/example/finalProject/Security/WebConfig.java
+++ finalProject/src/main/java/com/example/finalProject/Security/WebConfig.java
@@ -7,6 +7,10 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*").allowedOrigins("").allowedMethods("").allowedHeaders("");
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedMethods("PUT", "DELETE","POST","GET")
+                .allowedHeaders("header1", "header2", "header3")
+                .exposedHeaders("header1", "header2")
+                .allowCredentials(false).maxAge(3600);;
     }
 }
