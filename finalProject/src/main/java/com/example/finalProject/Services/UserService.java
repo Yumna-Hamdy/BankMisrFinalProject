e756bc24a28a0adc6e5d9eea81e9093dd4cda784
AliKhalaf1 <alikhalaf1234.ah@gmail.com>
2022-09-14 16:22:58 +0200

    fixing login

--- finalProject/src/main/java/com/example/finalProject/Services/UserService.java
+++ finalProject/src/main/java/com/example/finalProject/Services/UserService.java
@@ -32,7 +32,7 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) {
-        User user = userRepository.findByEmail(email).orElseThrow(()->new ResponseStatusException(HttpStatus.BAD_REQUEST,"user with same email not found"));
+        User user = userRepository.findByEmail(email).orElseThrow(()->new IllegalStateException("email not valid"));
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
     }
 
