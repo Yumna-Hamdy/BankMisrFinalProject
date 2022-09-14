8d2bd937fa6891d6559f1ac52d3f7fe3b819ae15
AliKhalaf1 <alikhalaf1234.ah@gmail.com>
2022-09-14 18:06:46 +0200

    fixing login

--- finalProject/src/main/java/com/example/finalProject/Controllers/UserController.java
+++ finalProject/src/main/java/com/example/finalProject/Controllers/UserController.java
@@ -3,30 +3,28 @@ package com.example.finalProject.Controllers;
 import com.example.finalProject.Entities.User;
 import com.example.finalProject.Services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping(path= "/user")
+@RequestMapping(value ="/user")
 @CrossOrigin(origins = "*")
 public class UserController {
-
-    private final UserService userService;
+    @Autowired
+    UserService userService;
 
     @PostMapping("/register")
     public User register(@RequestBody User user){
         return userService.register(user);
     }
-    @GetMapping(path = "confirm")
+    @GetMapping(path = "/confirm")
     public ResponseEntity<String> confirm(@RequestParam("token") String token) {
         return ResponseEntity.ok().body(userService.confirmToken(token));
     }
-    @GetMapping(path = "/test")
-    public String test(){
-        return "h3l";
-    }
+
 
 
 
